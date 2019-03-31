d7db9630a9ad8d2e4a6c2471a2f3fc8e0a2694ce
David zhang <d.ydzdavid@yahoo.com>
2019-03-31 04:45:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,17 +1,17 @@
 def consolidate_cart(cart)
   # code here
-  coupons.each do |coupon|
-    if cart.has_key?(coupon[:item]) && cart[coupon[:item]][:count] >= coupon[:num]
-      item_withcoupon = coupon[:item] + " W/COUPON"
-      if cart.has_key?(item_withcoupon)
-        cart[item_withcoupon][:count] += 1
-      else
-        cart[item_withcoupon] = {price: coupon[:cost], clearance: cart[coupon[:item]][:clearance], count: 1}
-      end
-      cart[coupon[:item]][:count] -= coupon[:num]
+  consolidated = {}
+cart.each do |item|
+  item.each do |key, value|
+    if consolidated.has_key?(key)
+      consolidated[key][:count] += 1
+    else
+      consolidated[key] = value
+      consolidated[key][:count] = 1
     end
   end
-  cart
+end
+consolidated
 end
   
   
