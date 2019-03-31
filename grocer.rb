4bb50a401253dd56225f886ee2ab9dfc330638e8
David zhang <d.ydzdavid@yahoo.com>
2019-03-31 03:00:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,18 @@
 def consolidate_cart(cart)
   # code here
+  new_cart = {}
+  cart.each do |item|
+    item.each do |key, value|
+      value[:count] = cart.count(item)
+      new_cart[key] = value
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  
 end
 
 def apply_clearance(cart)
